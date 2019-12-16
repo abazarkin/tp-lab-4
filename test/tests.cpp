9365e02154bc8c186f41436e369659eaeff0337d
AlNualD <nuald@outlook.com>
2019-12-16 18:57:15 +0300

    testing

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,4 +6,17 @@ TEST(lab4, check1)
 {
 	Automata Coffe;
 	EXPECT_EQ(OFF, Coffe.getState());
+	Coffe.on();
+	EXPECT_EQ(WAIT, Coffe.getState());
+	EXPECT_EQ(MISTAKE, Coffe.on());
+	Coffe.off();
+	EXPECT_EQ(OFF, Coffe.getState());
+	Coffe.~Automata
+}
+
+TEST(lab4, check2)
+{
+	Automata A;
+	A.on();
+	EXPECT_EQ(MISTAKE, A.choise(101));
 }
