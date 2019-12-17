c8bd57cf2e9d2fecfdd4c473f5e87bfa922bca8c
Klenkov <50375131+Klenkov@users.noreply.github.com>
2019-12-17 22:20:44 +0300

    lol

--- test/test.cpp
+++ test/test.cpp
@@ -4,30 +4,6 @@
 #include "gtest/gtest.h"
 #include "Automata.h"
 
-/*TEST(automata_on, test1){
-    Automata my_purchase;
-    my_purchase.on();
-    EXPECT_EQ(ON,my_purchase.printState());
-}
-
-TEST(automata_off, test2){
-    Automata my_purchase;
-    my_purchase.on();
-    my_purchase.ooin(100);
-    my_purchase.choice(2);
-    my_purchase.cook();
-    my_purchase.finish();
-    my_purchase.off()
-    EXPECT_EQ(OFF,my_purchase.printState());
-}
-
-TEST(automata_wrong_choice,test3){
-    Automata my_purchase;
-    my_purchase.on();
-    my_purchase.coin(100);
-    my_purchase.choice(6);
-    EXPECT_EQ(ERROR,my_purchase.printState());
-}*/
 
 TEST(automata_cash,test4){
     Automata my_purchase;
