7e05cba4bc427db555cae746bd8e0c08287773ee
Krylova Maria <krylova.maria.2629@gmail.com>
2019-12-17 20:29:11 +0300

    first tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,7 +10,7 @@ TEST(lab4, task_2) {
     automata.on();
 
     States tmp = automata.getState();
-    EXPECT_EQ(States::WAIT, tmp);
+    EXPECT_EQ(STATES::WAIT, tmp);
 }
 
 TEST(lab4, task_3) {
@@ -19,72 +19,5 @@ TEST(lab4, task_3) {
     automata.off();
 
     States tmp = automata.getState();
-    EXPECT_EQ(States::OFF, tmp);
-}
-
-TEST(lab4, task_4) {
-    Automata automata;
-    automata.on();
-
-    int tmp = automata.getCash();
-    EXPECT_EQ(0, tmp);
-}
-
-TEST(lab4, task_5) {
-    Automata automata;
-    automata.on();
-    automata.coin(12);
-
-    int tmp = automata.getCash();
-    EXPECT_EQ(12, tmp);
-}
-
-TEST(lab4, task_6) {
-    Automata automata;
-    automata.on();
-    automata.coin(12);
-
-    bool tmp = automata.choice(0);
-    EXPECT_EQ(false, tmp);
-}
-
-TEST(lab4, task_7) {
-    Automata automata;
-    automata.on();
-    automata.coin(12);
-
-    bool tmp = automata.choice(1);
-    EXPECT_EQ(true, tmp);
-}
-
-TEST(lab4, task_8) {
-    Automata automata;
-    automata.on();
-    automata.coin(12);
-    automata.choice(1);
-
-    int tmp = automata.getCash();
-    EXPECT_EQ(2, tmp);
-}
-
-TEST(lab4, task_9) {
-    Automata automata;
-    automata.on();
-    automata.coin(12);
-    automata.choice(1);
-    automata.coin(40);
-
-
-bool tmp = automata.choice(3);
-EXPECT_EQ(true, tmp);
-}
-
-TEST(lab4, task_10) {
-    Automata automata;
-    automata.on();
-    automata.coin(12);
-    automata.choice(1);
-
-    States tmp = automata.getState();
-    EXPECT_EQ(States::WAIT, tmp);
+    EXPECT_EQ(STATES::OFF, tmp);
 }
