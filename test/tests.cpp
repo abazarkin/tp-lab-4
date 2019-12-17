3433342978df189f978f168ea93a9920a43281c6
Krylova Maria <krylova.maria.2629@gmail.com>
2019-12-18 00:07:08 +0300

    tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -21,3 +21,34 @@ TEST(lab4, task_3) {
     STATES tmp = automata.getState();
     EXPECT_EQ(STATES::OFF, tmp);
 }
+TEST(lab4, task_4){
+    Automata automata;
+    automata.on();
+
+    int tmp = automata.getCash();
+    EXPECT_EQ(0, tmp);
+}
+TEST(lab4, task_5){
+    Automata automata;
+    automata.on();
+    automata.coin(20)
+
+    int tmp = automata.getCash();
+    EXPECT_EQ(20, tmp);
+}
+TEST(lab4, task_6){
+    Automata automata;
+    automata.on();
+    automata.coin(20)
+
+    bool tmp = automata.check(0);
+    EXPECT_EQ(false, tmp);
+}
+TEST(lab4, task_7){
+    Automata automata;
+    automata.on();
+    automata.coin(50)
+
+    bool tmp = automata.check(0);
+    EXPECT_EQ(true, tmp);
+}
