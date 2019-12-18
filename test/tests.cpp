2c4477b44fa1725d3288a6305a11e91e37190fc2
Abramov Alexander <52marba@mail.ru>
2019-12-19 00:09:34 +0300

    Done. Add some tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,8 +5,29 @@
 // task1
 TEST(test1,task1)
 {
+    //is this thing work
     Automata aut;
     aut.on();
     EXPECT_EQ(1,aut.getState());
 }
 
+TEST(test1,task2)
+{
+    //is this thing work
+    Automata aut;
+    aut.on();
+    aut.coin();
+    EXPECT_EQ(2,aut.getState());
+}
+
+TEST(test1,task2)
+{
+    //is this thing work
+    Automata aut;
+    aut.on();
+    aut.coin();
+    aut.choice();
+    aut.finish();
+    aut.off()
+    EXPECT_EQ(0,aut.getState());
+}
