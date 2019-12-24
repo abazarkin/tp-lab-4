5cdf9d9eea1cf35df7099a6faaac274c9e5ba149
IMashtakov <mashtakovigor@mail.ru>
2019-12-24 14:43:12 +0300

    try1

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "automata.h"
+int main()
+{
+	Automata mac = Automata();
+	mac.on();
+	mac.coin(10);
+	mac.coin(15);
+	mac.printMenu();
+	mac.choice(2);
+	mac.check();
+	mac.cook();
+	mac.finish();
+	mac.off();
+}
+
