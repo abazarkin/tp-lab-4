95cf4e7061347a6aaf9888fdb1bce645b70278cc
Milena <morozowamilwna@yandex.ru>
2019-12-24 13:44:18 +0300

    include "gtest/gtest.h"

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,7 @@
+#include "gtest/gtest.h"
 #include "CoffeeMachine.h"
 
-TEST(lab4, CoffeeMachine_) {
+TEST(lab4, Coffee) {
 	CoffeeMachine Philips = CoffeeMachine();
 	EXPECT_EQ(100, Philips.on());
 }
