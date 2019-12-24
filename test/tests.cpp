846ef9600665340ec195d7cbeccefbf495efb15f
Andrei Zhuravlev <dwasdx@gmail.com>
2019-12-24 22:33:24 +0300

    another fix to cmake

--- test/tests.cpp
+++ test/tests.cpp
@@ -84,7 +84,7 @@ TEST(Funcs_test, finish_test) {
 	EXPECT_EQ(aut.print_state(), "WAIT");
 }
 
-TEST(Menu_test) {
+TEST(Menu_test, menu_test) {
 	std::string str = "1: Water 10 R\n2: Soda 15 R\n3: Green tea 15 R\n4: Black tea 15 R\n5: Espresso 30 R\n6: Latte 45 R\n7: Cappuccino 50 R\n8: Double espresso 60 R\n9: Double latte 75 R\n10: Mocaccino 100 R\n";
 	Automata aut;
 	aut.on();
