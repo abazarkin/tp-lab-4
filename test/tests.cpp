9c01978a27375194d9ffa73e83973da0cb26e885
geniusserg <danilovsergeydmitrievich@gmail.com>
2019-12-24 18:52:36 +0300

    fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,14 +2,14 @@
 #include "automata.h"
 
 TEST (automata,test1){
-    Automata automata;
-    automata.on();
-    automata.coin(2000);
-    automata.off();
-    int odd_money = automata.cancel();
+    Automata* automata = new Automata();
+    automata->on();
+    automata->coin(2000);
+    automata->off();
+    int odd_money = automata->cancel();
     EXPECT_EQ(0,odd_money);
-    automata.on();
-    odd_money = automata.cancel();
+    automata->on();
+    odd_money = automata->cancel();
     EXPECT_EQ(2000,odd_money);
 }
 
