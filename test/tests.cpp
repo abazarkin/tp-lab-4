f013f5814bbabe57be50e1f5405651cfe84e0630
Олеся Мартынюк <olesyamartinyuk@gmail.com>
2019-12-24 23:19:36 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,38 +1,13 @@
 #include "gtest/gtest.h"
 #include "Automata.h"
 
-TEST(States_test, check_off_state) {
-   Automata aut;
-   EXPECT_EQ(aut.print_state(), "OFF");
- }
-
- TEST(States_test, check_on_state) {
-   Automata aut;
-   aut.on();
-   EXPECT_EQ(aut.print_state(), "WAIT");
- }
-
- TEST(States_test, check_off_func_state) {
-   Automata aut;
-   aut.on();
-   aut.off();
-   EXPECT_EQ(aut.print_state(), "OFF");
- }
-
- TEST(States_test, coin_test) {
+TEST(Cash_test, cash_check_test) {
    Automata aut;
    aut.on();
    aut.coin(50);
-   EXPECT_EQ(aut.print_state(), "ACCEPT");
- }
-
- TEST(Cash_test, cash_check_test) {
-   Automata aut;
-   aut.on();
-   aut.coin(50);
-   aut.choice(8);
-   EXPECT_EQ(aut.check(), false);
    aut.choice(4);
+   EXPECT_EQ(aut.check(), false);
+   aut.choice(1);
    EXPECT_EQ(aut.check(), true);
  }
 
@@ -40,48 +15,39 @@ TEST(States_test, check_off_state) {
    Automata aut;
    aut.on();
    aut.coin(50);
-   aut.choice(6);
+   aut.choice(2);
    EXPECT_EQ(aut.check(), true);
    aut.cook();
-   aut.choice(1);
-   EXPECT_EQ(aut.check(), false);
+   aut.choice(2);
+   EXPECT_EQ(aut.check(), true);
  }
 
- TEST(Cash_test, cash_after_buy_test2) {
-   Automata aut;
-   aut.on();
-   aut.coin(50);
-   aut.choice(6);
-   auto pair = aut.cook();
-   EXPECT_EQ(pair.first, 5);
- }
 
  TEST(Funcs_test, cancel_test1) {
    Automata aut;
    aut.on();
    aut.coin(50);
-   EXPECT_EQ(aut.print_state(), "ACCEPT");
+   EXPECT_EQ(state, "ACCEPT");
    aut.cancel();
-   EXPECT_EQ(aut.print_state(), "WAIT");
+   EXPECT_EQ(state, "WAIT");
  }
 
  TEST(Funcs_test, cancel_test2) {
    Automata aut;
    aut.on();
    aut.coin(50);
-   aut.choice(6);
+   aut.choice(1);
    aut.check();
-   EXPECT_EQ(aut.print_state(), "CHECK");
+   EXPECT_EQ(state, "CHECK");
  }
 
  TEST(Funcs_test, finish_test) {
    Automata aut;
    aut.on();
    aut.coin(50);
-   aut.choice(6);
+   aut.choice(3);
    aut.cook();
    aut.finish();
-   EXPECT_EQ(aut.print_state(), "WAIT");
+   EXPECT_EQ(state, "WAIT");
  }
 
- 
