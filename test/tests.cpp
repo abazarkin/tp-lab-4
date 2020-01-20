f734315e2c8534d2bc94312b24f77b07fc5ea9df
Aleksandr Komyagin <askomyagin@edu.hse.ru>
2020-01-20 21:56:21 +0300

    tttttt

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,24 +4,25 @@
 
 
 
-TEST(lab1, task1)
+TEST(TestS, task1)
 {
 	Automata automata;
 	automata.on();
-	EXPECT_EQ("WAIT", automata.printState());
+	EXPECT_EQ(20, automata..coin(20));
 	automata.off();
 }
-TEST(lab1,task2)
-{
-	Automata automata;
-	automata.coin(20);
-	EXPECT_EQ("ACCEPT",automata.printState());
-}
-TEST(lab1,task3)
+TEST(TestS,task2)
 {
 	Automata automata;
 	automata.on();
-	automata.coin(200);
-	automata.choice("Coffee");
-	EXPECT_EQ("OFF", automata.printState());
+	automata.coin(220);
+	EXPECT_EQ(320,automata.coin(100));
+	automata.off();
+}
+TEST(TestS,task3)
+{
+	Automata automata;
+	automata.on();
+	EXPECT_EQ("Coffee", automata.choice("Coffee"));
+	automata.off();
 }
