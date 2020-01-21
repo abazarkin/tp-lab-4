d12b0ff293c3523891363d145f24d82314b6f689
geniusserg <danilovsergeydmitrievich@gmail.com>
2020-01-21 08:36:14 +0300

    FIxTEST

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main() {
     automata.printMenu();
     automata.coin(5);
     automata.choice("apple");
-    automata.coin(123);
+    automata.coin(999);
     automata.choice("apple");
     string potato = automata.choice("potato");
     cout<< potato <<endl;
