fa009ac520313393db7ad2935425a97046f13751
Anton Korkunov <45770747+A1t3r@users.noreply.github.com>
2020-02-09 10:26:38 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,3 +1,8 @@
+//
+// Created by Anton Korkunov on 09/02/2020.
+//
+
+
 #include "Automata.h"
 #include <stdio.h>
 #include <string>
@@ -9,138 +14,134 @@ using namespace std;
 
 // 0 - off | 1 - wait | 2 - accept | 3 - check | 4 - cook //
 Automata::Automata() {
-	cash = 0;
-	state = 1;
-}//конструктор
+    cash = 0;
+    state = WAIT;
+}
 
 void Automata::on() {
-	if (state == 1) {
-		std::cout << "\n" << "Machine is already ON";
-		return;
-	}
-	state = 1;
-	printState();
-} //- включение автомата;
+    if (state == WAIT) {
+        std::cout << "\n" << "Machine is already ON";
+        return;
+    }
+    state = WAIT;
+    printState();
+}
 
 void Automata::off() {
-	std::cout << "\n" << "Have a nice day!";
-	state = 0;
-	printState();
-	//exit(1);
-}// - выключение автомата;
+    std::cout << "\n" << "Have a nice day!";
+    state = OFF;
+    printState();
+    //exit(1);
+}
 
 int Automata::coin(int _cash) {
-	state = 2;
-	if (_cash < 0) {
-		std::cout << "\n" << "Incorrect number!";
-		return -1;
-	}
-	printState();
-	if (_cash == 0) {
-		cancel();
-	}
-	this->cash += _cash;
-	return this->cash;
-} //- занесение денег на счёт пользователем;
+    state = ACCEPT;
+    if (_cash < 0) {
+        std::cout << "\n" << "Incorrect number!";
+        return -1;
+    }
+    printState();
+    if (_cash == 0) {
+        cancel();
+    }
+    this->cash += _cash;
+    return this->cash;
+}
 
 void Automata::printMenu() {
-	if (state != 0) {
-		std::cout << "Drink" << "|   |" << "Price";
-		for (int i = 0; i < 6; i++) {
-			std::cout << "\n" << i + 1 << ".|" << this->menu[i] << " |" << " - " << "| " << this->prices[i] << " |";
-		}
-	}
-} //- отображение меню с напитками и ценами для пользователя;
+    if (state != OFF) {
+        std::cout << "Drink" << "|   |" << "Price";
+        for (int i = 0; i < 6; i++) {
+            std::cout << "\n" << i + 1 << ".|" << this->menu[i] << " |" << " - " << "| " << this->prices[i] << " |";
+        }
+    }
+}
 
 int Automata::printState() {
-	if (state == 0) {
-		std::cout << "\n" << "OFF" << "\n";
-	}
+    if (state == OFF) {
+        std::cout << "\n" << "OFF" << "\n";
+    }
 
-	if (state == 1) {
-		std::cout << "\n" << "WAIT" << "\n";
-	}
+    if (state == WAIT) {
+        std::cout << "\n" << "WAIT" << "\n";
+    }
 
-	if (state == 2) {
-		std::cout << "\n" << "ACCEPT" << "\n";
-	}
+    if (state == ACCEPT) {
+        std::cout << "\n" << "ACCEPT" << "\n";
+    }
 
-	if (state == 3) {
-		std::cout << "\n" << "CHECK" << "\n";
-	}
+    if (state == CHECK) {
+        std::cout << "\n" << "CHECK" << "\n";
+    }
 
-	if (state == 4) {
-		std::cout << "\n" << "COOK" << "\n";
-	}
-	return(state);
-}// - отображение текущего состояния для пользователя;	
+    if (state == COOK) {
+        std::cout << "\n" << "COOK" << "\n";
+    }
+    return(state);
+}
 
 void Automata::choice() {
-	int choice;
-	int _cash;
-	if (state == 0) {
-		std::cout << "\n" << "The Machine is OFF";
-		return;
-	}
-	state = 1;
-	printState();
-	printMenu();
-	std::cout << "\n" << "Please, choose the number of drink you want ";
-	std::cin >> choice;
-	choice -= 1;
-	if ((choice > 5) or (choice < 0)) {
-		std::cout << "\n" << "You have inserted incorrect number";
-		return;
-	}
-	std::cout << "\n" << "Now insert cash or cancel by pressing 0: ";
-	std::cin >> _cash;
-	coin(_cash);
-	while (!(check(choice))) {
-		std::cout << "\n" << "Please, Add money or cancel by pressing 0: ";
-		std::cin >> _cash;
-		coin(_cash);
-	}
-	//if (check(choice)) {
-	cook();
-	state = 1;
-	finish();
-	//}
-} //- выбор напитка пользователем;
+    int choice;
+    int _cash;
+    if (state == OFF) {
+        std::cout << "\n" << "The Machine is OFF";
+        return;
+    }
+    state = WAIT;
+    printState();
+    printMenu();
+    std::cout << "\n" << "Please, choose the number of drink you want ";
+    std::cin >> choice;
+    choice -= 1;
+    if ((choice > 5) or (choice < 0)) {
+        std::cout << "\n" << "You have inserted incorrect number";
+        return;
+    }
+    std::cout << "\n" << "Now insert cash or cancel by pressing 0: ";
+    std::cin >> _cash;
+    coin(_cash);
+    while (!(check(choice))) {
+        std::cout << "\n" << "Please, Add money or cancel by pressing 0: ";
+        std::cin >> _cash;
+        coin(_cash);
+    }
+    //if (check(choice)) {
+    cook();
+    state = WAIT;
+    finish();
+    //}
+} //- ГўГ»ГЎГ®Г° Г­Г ГЇГЁГІГЄГ  ГЇГ®Г«ГјГ§Г®ГўГ ГІГҐГ«ГҐГ¬;
 
 bool Automata::check(int _i) {
-	state = 3;
-	printState();
-	if (prices[_i] > cash) {
-		std::cout << "\n" << "Not enough money";
-		return false;
-	}
-	else {
-		cash -= prices[_i];
-		return true;
-	}
-}// - проверка наличия необходимой суммы;
+    state = CHECK;
+    printState();
+    if (prices[_i] > cash) {
+        std::cout << "\n" << "Not enough money";
+        return false;
+    }
+    else {
+        cash -= prices[_i];
+        return true;
+    }
+}
 
 int Automata::cancel() {
-	std::cout << "\n" << "Take your money - " << cash;
-	return cash;
-	//off();
-}// - отмена сеанса обслуживания пользователем;
+    std::cout << "\n" << "Take your money - " << cash;
+    return cash;
+    //off();
+}
 
 void Automata::cook() {
-	state = 4;
-	printState();
-	std::cout << "\n" << "Your drink is preparing now";
-	std::cout << "\n" << ".";
-	//Sleep(1000);
-	std::cout << "\n" << "..";
-//	Sleep(2000);
-	std::cout << "\n" << "...";
-	//Sleep(3000);
-//	Beep(523, 350);
-	std::cout << "\n" << "Your drink is ready";
-}// - имитация процесса приготовления напитка;
+    state = COOK;
+    printState();
+    std::cout << "\n" << "Your drink is preparing now";
+    std::cout << "\n" << ".";
+    std::cout << "\n" << "..";
+    std::cout << "\n" << "...";
+    std::cout << "\n" << "Your drink is ready";
+}
 
 void Automata::finish() {
-	std::cout << "\n" << "Take your money - " << cash;
-	off();
-}// - завершение обслуживания пользователя.
+    std::cout << "\n" << "Take your money - " << cash;
+    off();
+}
