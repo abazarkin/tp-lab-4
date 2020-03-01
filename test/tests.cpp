c3a437ff3b5472e527a3e832b3f78f7ecab7c540
geniusserg <50704370+geniusserg@users.noreply.github.com>
2020-03-01 18:15:25 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,7 +9,7 @@ TEST (automata,test1_system){
 	EXPECT_FALSE( _off_.empty());
 	EXPECT_FALSE( _add_coin_message_.empty());
 	EXPECT_FALSE( _menu_names_.size() != _menu_prices_.size());
-	EXPECT_FALSE( _menu_names_.size() > 0);
+	EXPECT_TRUE( _menu_names_.size() > 0);
 	EXPECT_FALSE( _cancel_.empty());
 	EXPECT_FALSE( _choice_.empty());
 	EXPECT_FALSE( _not_enough_money_.empty());
