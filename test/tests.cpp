a0412b2f51343595358dbd0ae18a443347a3cdab
Artyom Pyanzin <aapyanzin@edu.hse.ru>
2020-03-05 18:26:09 +0300

    changed tests.cpp #2

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,20 +3,19 @@
 
 TEST(lab4, automata_1)
 {
-	Automata *atm = new Automata();
-	atm->on();
-	atm->coin(100);
-	bool flag = atm->check(0);
-	delete atm;
+	Automata atm;
+	atm.on();
+	atm.coin(100);
+	bool flag = atm.check(0);
 	EXPECT_EQ(flag, true);
 }
 
 TEST(lab4, automata_2) 
 {
-	Automata *atm = new Automata();
-	atm->on();
-	atm->coin(10);
-	bool flag = atm->check(3);
+	Automata atm;
+	atm.on();
+	atm.coin(10);
+	bool flag = atm.check(3);
 	delete atm;
 	EXPECT_EQ(flag, true);
 }
