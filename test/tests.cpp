5f0041863f0c44e570979d9e2d5ca458eaaf0152
unknown <nastenckavoronowa@yandex.ru>
2020-03-14 17:07:42 +0300

    second

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,6 @@
 #include "gtest/gtest.h"
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
-#include "task4.h"
-#include "task5.h"
+#include "Automata.cpp"
+
 
 // task1
 TEST(lab4,task1_1)
