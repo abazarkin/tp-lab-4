bb0a971501519fa530433183ff59b0ea54754e1d
chris389838 <52491727+chris389838@users.noreply.github.com>
2020-03-14 16:32:39 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,27 @@
 #include "gtest/gtest.h"
+#include "../include/Automata.h"
+
+TEST(lab4,task1)
+{
+    Automata atm;
+    atm.on();
+    EXPECT_EQ(0, atm.getMoney());
+}
+
+TEST(lab4,task2)
+{
+    Automata atm;
+    atm.on();
+    atm.coin(100);
+    atm.choice(2);
+    EXPECT_EQ(30, atm.getMoney());
+}
+
+TEST(lab4,task3)
+{
+    Automata atm;
+    atm.on();
+    atm.coin(100);
+    atm.choice(3);
+    EXPECT_EQ(10, atm.getMoney());
+}
